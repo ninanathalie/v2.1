7f700b7e907b7968bde42460d6ae0330ec847b83
nathalie <mninanathalie@gmail.com>
2024-11-23 16:24:01 +0400

    chore(about): remove 'other skills' component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import AboutMe from "@/components/about/about";
 import KeySkills from "@/components/about/key-skills";
-import OtherSkills from "@/components/about/other-skills";
 
 export default function About() {
   return (
     <main>
       <AboutMe />
       <KeySkills />
-      <OtherSkills />
     </main>
   );
 }
